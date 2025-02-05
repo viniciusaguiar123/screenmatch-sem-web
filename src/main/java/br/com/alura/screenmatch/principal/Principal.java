05483136ae6f1d9aae04e5b3429f708debe67945
Vinicius Aguiar <vini1068@gmail.com>
2025-02-04 21:18:24 -0300

    FEAT: Coletando estatísticas

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -94,5 +94,14 @@ public class Principal {
                 .collect(Collectors.groupingBy(Episodio::getTemporada,
                         Collectors.averagingDouble(Episodio::getAvaliacao)));
         System.out.println(avaliacoesPorTemporada);
+
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(episodio -> episodio.getAvaliacao() > 0.0)
+                .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+
+        System.out.println("Média: " + est.getAverage());
+        System.out.println("Melhor episódio: " + est.getMax());
+        System.out.println("Pior episódio: " + est.getMin());
+        System.out.println("Quantidade: " + est.getCount());
     }
 }
